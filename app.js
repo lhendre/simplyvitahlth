b3b368758b75d467325f49c615d6c225d169d78d
Katherine Kuzmeskas <katherinekuzmeskas@Matthew-Kuzmeskass-MacBook-Pro.local>
2016-10-01 12:16:25 -0500

    added files

---
+++ app.js
@@ -0,0 +1,21 @@
+// set variables for environment
+var express = require('express');
+var app = express();
+var path = require('path');
+
+
+
+// views as directory for all template files
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs'); // use either jade or ejs
+// instruct express to server up static assets
+app.use(express.static('public'));
+
+
+// set routes
+app.get('/', function(req, res) {
+  res.render('index');
+});
+// Set server port
+app.listen(4000);
+console.log('server is running');
---
+++ gruntfile.js
@@ -0,0 +1,22 @@
+module.exports = function(grunt) {
+  grunt.initConfig ({
+    sass: {
+      dist: {
+        files: {
+          'public/stylesheets/style.css' : 'sass/style.scss'
+        }
+      }
+    },
+    watch: {
+      source: {
+        files: ['sass/**/*.scss'],
+        tasks: ['sass'],
+        options: {
+          livereload: true, // needed to run LiveReload
+        }
+      }
+    }
+
+  grunt.loadNpmTasks('grunt-sass');
+  grunt.registerTask('default', ['sass']);
+};
