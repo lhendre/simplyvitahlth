adcfec5438a48170465ed9df425c7649460157f7
Paul Cuciureanu <paulcu@gmail.com>
2016-10-01 23:29:30 -0400

    Adding web sockets

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var express = require('express');
 var app = express();
 var path = require('path');
 
-
+var expressWs = require('express-ws')(app);
 
 // views as directory for all template files
 app.set('views', path.join(__dirname, 'views'));
@@ -16,6 +16,31 @@ app.use(express.static('public'));
 app.get('/', function(req, res) {
   res.render('index');
 });
+
+app.get("/visits", function(req, res) {
+  var visitsArray = {
+    "data": [
+      {
+        "date": "2011/04/25",
+        "name": "Joe Ziggeler",
+        "notes": "Doing great, gave new meds, alergies galore",
+        "provider": "Bones Orthopaedics",
+        "procedure": "Evaluation & Management",
+        "practice": "Michael Bones MD",
+        "specialty": "Orthopaedic surgery",
+        "priority": 1
+      }
+    ]
+  }
+  res.send(visitsArray);
+});
+
+app.ws('/echo', function(ws, req) {
+  ws.on('message', function(msg) {
+    ws.send('Pong ' + msg);
+  });
+});
+
 app.use('/blockchain',require('./controllers/blockchain'));
 // Set server port
 app.listen(4000);
